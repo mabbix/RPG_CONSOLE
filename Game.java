a24c4bcfe46368d865cc6ce09cc5deb3e2b8130b
Mabi <carvalhomabii@gmail.com>
2022-01-09 19:54:28 -0300

    teste1

--- Game.java
+++ Game.java
@@ -16,16 +16,16 @@ public class Game {
 		System.out.println("		    Bem vindo ao " + settings.ServerName);
 		System.out.println("=============================================================");
 		System.out.println("");
-		System.out.println("Informações sobre o GAME:");
+		System.out.println("Informacoes sobre o GAME:");
 		System.out.println("-------------------------------");
-		System.out.println("Versão atual: " + settings.ServerVersion);
+		System.out.println("Versao atual: " + settings.ServerVersion);
 		System.out.println("Taxa de EXP atual: " + settings.ExpRate);
 		System.out.printf("Double Exp atualmente esta: "); settings.settings();
 		System.out.println("");
 		
 		
 		//System.out.println("1-Crie Sua Conta ");
-		System.out.println("1-Faça login ");
+		System.out.println("1-Faca login ");
 		
 		Scanner sc = new Scanner(System.in);
 		Integer ToLogin = sc.nextInt();
--- entities/Accounts.java
+++ entities/Accounts.java
@@ -2,7 +2,7 @@ package entities;
 
 import java.util.Scanner;
 public final class Accounts {
-	
+	//teste1
 	
 	//Players manual add
 
